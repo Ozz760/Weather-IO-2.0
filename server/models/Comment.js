b67c78603690c6b14025d4c801ba4ab5477d7809
Oscar Silva <oscarsilva2356@gmail.com>
2022-06-09 09:29:35 -0700

    Made the comment model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,8 +1,23 @@
 const { Schema, model } = require("mongoose");
 
 const commentSchema = new Schema({
+  commentAuthor: {
+    type: String,
+    require: true,
+  },
   comment: {
-    type: Text,
+    type: String,
     required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  Date: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
   },
 });
+
+const Comment = model("Comment", commentSchema); 
+
+module.exports = Comment; 
