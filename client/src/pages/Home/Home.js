acb671727e4c97ccd0c6f50225dd267071f4f82e
Oscar Silva <oscarsilva2356@gmail.com>
2022-06-22 09:26:01 -0700

    add imperial units to the weather api

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,7 +3,7 @@ const apiKey = "e4d5971bc32e69580b23b38529ab190e";
 
 
 function weatherCall (cityName) {
-  const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+  const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",us&appid=" + apiKey + "&units=imperial";
   fetch (weatherUrl)
   .then(function(responce) {
     return responce.json(); 
