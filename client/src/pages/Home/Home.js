4b93520607470f7d68f6bf553643642d2158dc0f
Oscar Silva <oscarsilva2356@gmail.com>
2022-07-01 09:53:27 -0700

    made the fetch api catch error if any

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,7 @@
 import { useAuth } from "../../util/auth";
 import weatherCall from "../../util/weather";
 import { useState, useEffect } from "react";
-const apiKey = "e4d5971bc32e69580b23b38529ab190e"; 
-
-
+const apiKey = "e4d5971bc32e69580b23b38529ab190e";
 
 export default function Home() {
   const [data, setData] = useState(null);
@@ -12,10 +10,23 @@ export default function Home() {
 
   useEffect(() => {
     //fetching data from the api
-    fetch ("https://api.openweathermap.org/data/2.5/weather?q=SanDiego,us&appid=" + apiKey + "&units=imperial")
-    .then((response) => console.log(response));
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=SanDiego,us&appid=" +
+        apiKey +
+        "&units=imperial"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Status Code: ${response.status}`); //if the response is not ok, throw an error
+        }
+        return response.json(); //if the response is ok, return the json
+      })
+      .then((actualData) => console.log(actualData))
+      .catch((err) => {
+        console.log(err.message);
+      }); //end of fetch
   }, []);
-  
+
   const { isLoggedIn, user } = useAuth();
   return (
     <div>
@@ -29,18 +40,23 @@ export default function Home() {
         make comments yourself.
       </p>
       <form id="search-form">
-        <input type="text" id="search-input" className="form-input w-100 fs-3" placeholder="Enter a city name" />
-        <button type="submit" id="submit button" className="col-12 bg-info fs-4 my-4 p-2 border-dark">
-          Submit 
+        <input
+          type="text"
+          id="search-input"
+          className="form-input w-100 fs-3"
+          placeholder="Enter a city name"
+        />
+        <button
+          type="submit"
+          id="submit button"
+          className="col-12 bg-info fs-4 my-4 p-2 border-dark"
+        >
+          Submit
         </button>
       </form>
       <div>
-        <h3>
-          Weather Info
-        </h3>
-        <p>
-          Location: 
-        </p>
+        <h3>Weather Info</h3>
+        <p>Location:</p>
       </div>
     </div>
   );
